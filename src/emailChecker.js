1fa83de6b7336bed03f4803e1964661dfdc9f2a2
pb72yvp <143070157+Hugo652-droid@users.noreply.github.com>
2024-09-12 14:16:27 +0200

    feat:Exersice labo 2 semain 3 emailChecker

--- src/emailChecker.js
+++ src/emailChecker.js
@@ -2,7 +2,7 @@
 class EmailValidationError extends Error {
     constructor(message) {
         super(message);  // Appelle le constructeur de la classe parent (Error)
-        //TODO
+        this.name = "EmailvalidationError";
     }
 }
 
@@ -12,7 +12,7 @@ function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!emailRegex.test(email)) {
-        //TODO
+        throw new EmailValidationError("Il faut que le mail corésponde au critaire.");
     }
     return "Adresse e-mail valide.";
 }
